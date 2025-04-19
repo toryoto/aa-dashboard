5851972fe1b9e5b58f53098d9562788b3c46c1da
toryoto <ryoto.taga12@gmail.com>
2025-04-19 18:46:27 +0900

    docker

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,10 +4,7 @@ import cors from 'cors'
 
 const app = express()
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  process.env.FRONTEND_URL,
-]
+const allowedOrigins = ['http://localhost:3000', process.env.FRONTEND_URL]
 
 const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
