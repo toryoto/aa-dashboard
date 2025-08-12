6726907e0c8d00702635893292879e61cb658f9e
toryoto <ryoto.taga12@gmail.com>
2025-08-12 21:48:31 +0900

    lintter

--- aa-front/app/utils/packUserOperation.ts
+++ aa-front/app/utils/packUserOperation.ts
@@ -3,9 +3,9 @@ import { PackedUserOperation } from '../lib/userOperationType'
 
 // 2つの数値を32バイトずつパックしてHex文字列として返すユーティリティ関数
 const packUints = (a: number, b: number): Hex => {
-    // aを下位128ビット、bを上位128ビットに配置
-    return `0x${(BigInt(a) | (BigInt(b) << BigInt(128))).toString(16).padStart(64, '0')}` as Hex
-  }
+  // aを下位128ビット、bを上位128ビットに配置
+  return `0x${(BigInt(a) | (BigInt(b) << BigInt(128))).toString(16).padStart(64, '0')}` as Hex
+}
 
 export const createPackedUserOperation = (params: {
   sender: Hex
