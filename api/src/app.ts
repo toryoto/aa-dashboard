7377d563521ad47f37ddd8a004516313ebf1d5b8
toryoto <ryoto.taga12@gmail.com>
2025-09-27 16:20:02 +0900

    CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,20 +6,20 @@ import userOpRouter from './routes/userOp'
 
 const app = express()
 
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://aa-dashboard-frontend-429781279541.asia-northeast1.run.app',
-//   'https://aa-dashboard.xyz',
-// ]
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://aa-dashboard-frontend-429781279541.asia-northeast1.run.app',
+  'https://aa-dashboard.xyz',
+]
 
 const corsOptions: cors.CorsOptions = {
-  // origin: function (origin, callback) {
-  //   if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-  //     callback(null, true)
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'))
-  //   }
-  // },
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   methods: 'GET, POST, PUT, DELETE, OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   credentials: true,
